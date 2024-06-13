a44aafe146d89a6bbdb1518c92dc5b4ccb7cf5a8
Martin Stancsics <martin.stancsics@gmail.com>
2024-06-13 16:24:40 +0200

    Add term-frequency based classifier

--- src/analysis/chat.py
+++ src/analysis/chat.py
@@ -2,23 +2,18 @@ import re
 import string
 
 import nltk
-import numpy as np
 import polars as pl
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
 
 
 def prepare_dataset(outcomes: pl.DataFrame, actions: pl.DataFrame) -> pl.DataFrame:
-    player_names = pl.Enum(["P1", "P2", "P3"])
     agreement_types = pl.Enum(["Breakdown", "Partial agreement", "Full agreement"])
-    roles = {
-        1: "P1",
-        2: "P2",
-        3: "P3",
-    }
-
     treatment_names = pl.Enum(
         [
             "Dummy player",
@@ -29,82 +24,68 @@ def prepare_dataset(outcomes: pl.DataFrame, actions: pl.DataFrame) -> pl.DataFra
     )
 
     chat = (
-        pl.read_csv("data/clean/_collected/actions.csv")
-        .filter(pl.col("action").eq("chat"))
-        .select(
-            "session_code",
-            "treatment_name",
-            "round_number",
-            "group_id",
-            "id_in_group",
-            message="body",
-        )
-    ).with_columns(
-        treatment_name_nice=pl.col("treatment_name")
-        .replace(
-            {
-                "treatment_dummy_player": "Dummy player",
-                "treatment_y_10": "Y = 10",
-                "treatment_y_30": "Y = 30",
-                "treatment_y_90": "Y = 90",
-            }
-        )
-        .cast(treatment_names),
-    )
-
-    outcome_properties = outcomes.filter(
-        pl.col("round_number") > 1,
-    ).with_columns(
-        role=pl.col("id_in_group").replace(roles).cast(player_names),
-        agreement=(
-            pl.when(
-                pl.col("payoff_this_round")
-                .sum()
-                .over(["session_code", "round_number", "group_id"])
-                == 0
-            )
-            .then(pl.lit("Breakdown"))
-            .otherwise(
-                pl.when(
-                    (pl.col("payoff_this_round") > 0)
-                    .all()
-                    .over(["session_code", "round_number", "group_id"])
-                )
-                .then(pl.lit("Full agreement"))
-                .otherwise(pl.lit("Partial agreement"))
-            )
-        ).cast(agreement_types),
-        equal_split=(
-            pl.col("payoff_this_round")
-            .max()
-            .over(["session_code", "round_number", "group_id"])
-            - pl.col("payoff_this_round")
-            .min()
-            .over(["session_code", "round_number", "group_id"])
-        )
-        <= 1,
-    )
-
-    chat_with_outcomes = chat.filter(pl.col("round_number") > 1).join(
-        outcome_properties.select(
-            [
+        (
+            pl.read_csv("data/clean/_collected/actions.csv")
+            .filter(pl.col("action").eq("chat"))
+            .select(
                 "session_code",
+                "treatment_name",
                 "round_number",
                 "group_id",
                 "id_in_group",
-                "payoff_this_round",
-                "agreement",
-                "equal_split",
-            ]
-        ),
-        on=["session_code", "round_number", "group_id", "id_in_group"],
+                message="body",
+            )
+        )
+        .with_columns(
+            treatment_name_nice=pl.col("treatment_name")
+            .replace(
+                {
+                    "treatment_dummy_player": "Dummy player",
+                    "treatment_y_10": "Y = 10",
+                    "treatment_y_30": "Y = 30",
+                    "treatment_y_90": "Y = 90",
+                }
+            )
+            .cast(treatment_names),
+        )
+        .group_by(["session_code", "round_number", "group_id"])
+        .agg(pl.col("message").str.concat(" | ").alias("message"))
+    )
+
+    outcome_properties = (
+        outcomes.filter(
+            pl.col("round_number") > 1,
+        )
+        .group_by(["session_code", "round_number", "group_id"])
+        .agg(
+            agreement=(
+                pl.when(pl.col("payoff_this_round").sum() == 0)
+                .then(pl.lit("Breakdown"))
+                .otherwise(
+                    pl.when((pl.col("payoff_this_round") > 0).all())
+                    .then(pl.lit("Full agreement"))
+                    .otherwise(pl.lit("Partial agreement"))
+                )
+            ).cast(agreement_types),
+            equal_split=(
+                (pl.col("payoff_this_round").max() - pl.col("payoff_this_round").min())
+                <= 1
+            )
+            & pl.col("payoff_this_round").sum()
+            != 0,
+        )
+    )
+
+    chat_with_outcomes = chat.filter(pl.col("round_number") > 1).join(
+        outcome_properties,
+        on=["session_code", "round_number", "group_id"],
         how="left",
     )
 
     return chat_with_outcomes
 
 
-def train_idf(df: pl.DataFrame, vectorizer_args={}) -> TfidfVectorizer:
+def make_tfifd_transformer() -> TfidfVectorizer:
     class LemmaTokenizer:
         def __init__(self):
             self.wnl = WordNetLemmatizer()
@@ -121,36 +102,24 @@ def train_idf(df: pl.DataFrame, vectorizer_args={}) -> TfidfVectorizer:
 
     vectorizer = TfidfVectorizer(
         tokenizer=LemmaTokenizer(),
-        **vectorizer_args,
     )
-    vectorizer.fit(df["message"])
     return vectorizer
 
 
-def get_tfidf_matrix(
-    df: pl.DataFrame, vectorizer: TfidfVectorizer, group_var: str
-) -> tuple[np.ndarray, list]:
-    documents = (
-        df.group_by(group_var)
-        .agg(pl.col("message").str.concat(" ").alias("message"))
-        .sort(group_var)
-    )
-    groups = documents[group_var].to_list()
-    tfidf_matrix = vectorizer.transform(documents["message"]).toarray()  # type: ignore
-
-    return tfidf_matrix, groups
+def make_classifier() -> XGBClassifier:
+    return XGBClassifier(enable_categorical=True)
 
 
-def get_top_k_words(
-    df: pl.DataFrame, vectorizer: TfidfVectorizer, group_var: str, k: int
-):
-    tfidf_matrix, groups = get_tfidf_matrix(df, vectorizer, group_var)
-    top_k_word_indices = np.argsort(tfidf_matrix, axis=1)[:, -k:][:, ::-1]
+def make_classification_pipeline() -> Pipeline:
+    return make_pipeline(make_tfifd_transformer(), make_classifier())
 
-    words = vectorizer.get_feature_names_out()
-    top_k_words = words[top_k_word_indices]
 
-    return {group: list(top_words) for group, top_words in zip(groups, top_k_words)}
+def fit_classifier(df: pl.DataFrame, group_var: str) -> tuple[Pipeline, LabelEncoder]:
+    pipeline = make_classification_pipeline()
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(df[group_var])
+    pipeline.fit(df["message"], y)  # type: ignore
+    return pipeline, label_encoder
 
 
 def setup_nltk() -> None:
@@ -166,4 +135,4 @@ if __name__ == "__main__":
 
     setup_nltk()
     df = prepare_dataset(outcomes, actions)
-    vectorizer = train_idf(df)
+    pipeline, label_encoder = fit_classifier(df, "agreement")
