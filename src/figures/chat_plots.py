a5f5839960bf95e8ce44cc1214f27c98107ca504
Martin Stancsics <martin.stancsics@gmail.com>
2024-06-14 15:52:23 +0200

    Add code for finding top  words per groups

---
+++ src/figures/chat_plots.py
@@ -0,0 +1,169 @@
+import polars as pl
+import spacy
+
+
+def prepare_dataset(outcomes: pl.DataFrame, actions: pl.DataFrame) -> pl.DataFrame:
+    player_names = pl.Enum(["P1", "P2", "P3"])
+    agreement_types = pl.Enum(["Breakdown", "Partial agreement", "Full agreement"])
+    roles = {
+        1: "P1",
+        2: "P2",
+        3: "P3",
+    }
+    treatment_names = pl.Enum(
+        [
+            "Dummy player",
+            "Y = 10",
+            "Y = 30",
+            "Y = 90",
+        ]
+    )
+
+    chat = (
+        pl.read_csv("data/clean/_collected/actions.csv")
+        .filter(pl.col("action").eq("chat"))
+        .select(
+            "session_code",
+            "treatment_name",
+            "round_number",
+            "group_id",
+            "id_in_group",
+            message="body",
+        )
+    ).with_columns(
+        treatment_name_nice=pl.col("treatment_name")
+        .replace(
+            {
+                "treatment_dummy_player": "Dummy player",
+                "treatment_y_10": "Y = 10",
+                "treatment_y_30": "Y = 30",
+                "treatment_y_90": "Y = 90",
+            }
+        )
+        .cast(treatment_names),
+    )
+
+    outcome_properties = (
+        outcomes.filter(
+            pl.col("round_number") > 1,
+        )
+        .with_columns(
+            max_payoff=pl.col("payoff_this_round")
+            .max()
+            .over(["session_code", "round_number", "group_id"]),
+            min_payoff=pl.col("payoff_this_round")
+            .min()
+            .over(["session_code", "round_number", "group_id"]),
+            total_payoff=pl.col("payoff_this_round")
+            .sum()
+            .over(["session_code", "round_number", "group_id"]),
+        )
+        .with_columns(
+            role=pl.col("id_in_group").replace(roles).cast(player_names),
+            agreement=(
+                pl.when(pl.col("total_payoff") == 0)
+                .then(pl.lit("Breakdown"))
+                .otherwise(
+                    pl.when(pl.col("min_payoff") > 0)
+                    .then(pl.lit("Full agreement"))
+                    .otherwise(pl.lit("Partial agreement"))
+                )
+            ).cast(agreement_types),
+            equal_split=(
+                (pl.col("max_payoff") - pl.col("min_payoff") <= 1)
+                & (pl.col("total_payoff") > 0)
+            ),
+        )
+    )
+
+    chat_with_outcomes = chat.filter(pl.col("round_number") > 1).join(
+        outcome_properties.select(
+            [
+                "session_code",
+                "round_number",
+                "group_id",
+                "id_in_group",
+                "payoff_this_round",
+                "agreement",
+                "equal_split",
+            ]
+        ),
+        on=["session_code", "round_number", "group_id", "id_in_group"],
+        how="left",
+    )
+
+    return chat_with_outcomes
+
+
+def lemmatize_chat(chat: pl.DataFrame, model: spacy.Language) -> pl.DataFrame:  # type: ignore
+    def lemmatize(text: str) -> list[dict[str, str]]:
+        nlp = model(text)
+        return [
+            {
+                "lemma": token.lemma_,
+                "pos": token.pos_,
+            }
+            for token in nlp
+        ]
+
+    return (
+        chat.with_columns(
+            words=pl.col("message").apply(
+                lemmatize,
+                return_dtype=pl.List(pl.Struct({"lemma": pl.Utf8, "pos": pl.Utf8})),
+            ),
+        )
+        .explode("words")
+        .unnest("words")
+        .filter(pl.col("lemma").is_not_null())
+    )
+
+
+def count_words(
+    df: pl.DataFrame, predicament: pl.Expr, word_type: str | None = None
+) -> pl.DataFrame:
+    if word_type is not None:
+        df = df.filter(pl.col("pos") == word_type)
+
+    word_counts = (
+        df.with_columns(
+            group_var=predicament,
+        )
+        .groupby(["group_var", "lemma"])
+        .agg(
+            count=pl.count("lemma"),
+        )
+        .with_columns(
+            total_count=pl.count("lemma").over("group_var"),
+        )
+        .with_columns(
+            freq=pl.col("count") / pl.col("total_count"),
+        )
+        .pivot(values="freq", index="lemma", columns="group_var")
+        .fill_null(0)
+        .with_columns(
+            relative_freq=pl.col("true") - pl.col("false"),
+            total_freq=pl.col("true") + pl.col("false"),
+        )
+    )
+
+    return word_counts
+
+
+def setup_spacy(model_name) -> spacy.Language:  # type: ignore
+    try:
+        model = spacy.load(model_name)
+    except OSError:
+        spacy.cli.download(model_name)  # type: ignore
+        model = spacy.load(model_name)
+
+    return model
+
+
+if __name__ == "__main__":
+    outcomes = pl.read_csv(snakemake.input.outcomes)  # noqa F821 # type: ignore
+    actions = pl.read_csv(snakemake.input.actions)  # noqa F821 # type: ignore
+
+    model = setup_spacy("en_core_web_sm")
+    df = prepare_dataset(outcomes, actions)
+    df_processed = lemmatize_chat(df, model)
