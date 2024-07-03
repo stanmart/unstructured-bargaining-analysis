4accad8c21f1f0cc9d0450417c80ee92b031e5bf
Martin Stancsics <martin.stancsics@gmail.com>
2024-07-03 16:04:30 +0200

    Simplify snakemake rule for presentations

---
+++ src/util/makeutils.py
@@ -0,0 +1,17 @@
+import re
+
+
+def find_quarto_images(quarto_file: str, remove_leading_slash=True):
+    with open(quarto_file, "r") as file:
+        manim_contents = file.read()
+
+    patterns = [
+        re.compile(r"\!\[.*?\]\((.*?)\)"),
+    ]
+
+    images = sum((pattern.findall(manim_contents) for pattern in patterns), [])
+
+    if remove_leading_slash:
+        images = [section.lstrip("/") for section in images]
+
+    return images
