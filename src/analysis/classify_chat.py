3db61f5b2b7ee6bf60eb3886a1faa8ce1b8d8292
Martin Stancsics <martin.stancsics@gmail.com>
2024-07-12 18:15:25 +0200

    Reorganize caching

--- src/analysis/classify_chat.py
+++ src/analysis/classify_chat.py
@@ -1,5 +1,5 @@
+import glob
 import os
-from pathlib import Path
 from typing import Any
 
 import openai
@@ -16,13 +16,11 @@ separated by newlines.
 
 Please classify which TOPIC each message (MSG) belongs to. You only have to classify messages, not proposals or acceptances (those latter two are only included for context). Possible topics are: bargaining-related, small-talk, experiment-related, fairness-related, meta-talk, identifying-each-other. The classification should also take into account the context of the message.
 
-Your response should look like the following:
+Your response be a json object with the following format:
 #[MESSAGE_ID]: [TOPIC]
 for each message, separated by newlines.
+If there are no rows of type MSG, please respond with NO_MESSAGES.
 Please do not return anything else.
-
-Chat log:
-
 """
 
 
@@ -105,9 +103,13 @@ def get_model_response(client: openai.Client, chat_log: str) -> str:
     response = client.chat.completions.create(
         messages=[
             {
+                "role": "system",
+                "content": BASE_PROMPT,
+            },
+            {
                 "role": "user",
-                "content": BASE_PROMPT + chat_log,
-            }
+                "content": chat_log,
+            },
         ],
         model="gpt-3.5-turbo",
         temperature=0.0,
@@ -128,9 +130,13 @@ def parse_model_response_row(row: str) -> dict[str, int | str]:
 
 
 def parse_model_response(model_response: str) -> pl.DataFrame:
-    rows = model_response.split("\n")
+    model_response = model_response.strip()
+    if model_response == "NO_MESSAGES":
+        rows = []
+    else:
+        rows = (parse_model_response_row(row) for row in model_response.split("\n"))
     return pl.DataFrame(
-        (parse_model_response_row(row) for row in rows),
+        rows,
         schema={
             "action_id": pl.UInt32,
             "topic": pl.String,
@@ -143,58 +149,70 @@ def analyze_log(client: openai.Client, df: pl.DataFrame) -> pl.DataFrame:
     model_response = get_model_response(client, chat_log)
     model_response_df = parse_model_response(model_response)
 
-    result_df = (
-        df.filter(pl.col("action") == "chat")
-        .join(model_response_df, on="action_id")
-        .select(
-            [
-                "treatment_name",
-                "round_number",
-                "group_id",
-                "id_in_group",
-                "timestamp",
-                "body",
-                "topic",
-            ]
-        )
+    result_df = model_response_df.join(
+        df.filter(pl.col("action") == "chat"), on="action_id", how="left"
+    ).select(
+        [
+            "treatment_name",
+            "round_number",
+            "group_id",
+            "id_in_group",
+            "timestamp",
+            "body",
+            "topic",
+        ]
     )
 
     return result_df
 
 
 if __name__ == "__main__":
-    output_file = Path(snakemake.output[0])  # noqa F821 # type: ignore
-    output_exists = output_file.is_file()
+    cache_files = glob.glob(
+        os.path.join(snakemake.params["cache_dir"], "*.parquet")  # noqa F821 # type: ignore
+    )
     execution_allowed = os.environ.get("ALLOW_OPENAI_REQUESTS") == "true"
 
-    if output_exists:
-        print("Output file already exists, but Snakemake wants to recreate it.")
-        if not execution_allowed:
-            print("OpenAI requests are disabled. Using existing file.")
-            output_file.touch()
-            exit(0)
-        else:
-            print("OpenAI requests are enabled. Recreating file.")
+    if execution_allowed:
+        print("OpenAI requests are enabled. (Re)creating cache.")
+        if cache_files:
+            print("Cache directory is not empty. Deleting cache files.")
+            for file in cache_files:
+                os.remove(file)
+
+        os.makedirs(snakemake.params["cache_dir"], exist_ok=True)  # noqa F821 # type: ignore
+
+        actions = pl.read_csv(snakemake.input.actions)  # noqa F821 # type: ignore
+
+        df = prepare_dataset(actions)
+
+        for (treatment_name, group_id, round_number), dfi in df.group_by(  # type: ignore
+            "treatment_name", "group_id", "round_number"
+        ):
+            print(
+                f"Classifying chat messages for treatment {treatment_name}, round {round_number}, group {group_id}."
+            )
+            analyze_log(create_openai_client(), dfi).write_parquet(
+                os.path.join(
+                    snakemake.params["cache_dir"],  # noqa F821 # type: ignore
+                    f"{treatment_name}_{group_id}_{round_number}.parquet",
+                )
+            )
+
+        print("Done classyfing chat messages.")
+        print(
+            "Consider committing the cache directory to avoid unnecessary OpenAI requests."
+        )
+
     else:
-        print("Output file does not exist.")
-        if not execution_allowed:
-            print("OpenAI requests are disabled. Exiting.")
-            exit(1)
+        print("OpenAI requests are disabled.")
+        if cache_files:
+            print(f"Using {len(cache_files)} cached files.")
         else:
-            print("OpenAI requests are enabled. Creating file.")
+            print("No cache files found. Exiting.")
+            exit(1)
 
-    actions = pl.read_csv(snakemake.input.actions)  # noqa F821 # type: ignore
-
-    df = prepare_dataset(actions)[:100]
-
-    result_dfs = []
-    for _, dfi in df.group_by("treatment_name", "group_id", "round_number"):
-        result_dfs.append(analyze_log(create_openai_client(), dfi))
-
-    result_df_long = pl.concat(result_dfs)
-    result_df_long.write_csv(output_file)
-
-    print("Done classyfing chat messages.")
-    print(
-        "Consider committing the output to the reository to avoid unnecessary OpenAI requests."
+    new_cache_files = glob.glob(
+        os.path.join(snakemake.params["cache_dir"], "*.parquet")  # noqa F821 # type: ignore
     )
+    result_df_long = pl.concat(pl.read_parquet(file) for file in cache_files)
+    result_df_long.write_csv(snakemake.output.chat_classified)  # noqa F821 # type: ignore
